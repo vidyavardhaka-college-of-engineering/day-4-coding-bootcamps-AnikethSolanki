f502a3059cbf6fa0e617c658960bb73dd3fb147d
AnikethSolanki <78413185+AnikethSolanki@users.noreply.github.com>
2021-09-02 06:01:25 +0000

    program 5

--- Program_5.c
+++ Program_5.c
@@ -6,11 +6,17 @@
 int main()
 {
     int arr[10], n, i;
+    printf("enter the number of elements");
     scanf("%d", &n);
-
+    printf("enter the array elements");
     for(i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
+        small=large=arr[0];
+    if(arr[i]<small)
+    small=arr[i];
+    if(arr[i]>large)
+    large=arr[i];
+    printf("the largest and smallest elements are %d and %d",large,small);
     //Write your code here
   
     return 0;
